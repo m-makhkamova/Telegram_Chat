ed5582d116fb0312cf367c860eb8f4c0597ecf2c
User_3 <rmuhammadqodir58@gmail.com>
2023-12-04 09:54:24 +0500

    Initial commit

---
+++ app/src/main/java/uz/itschool/telegram_chat/navigation/Screens.kt
@@ -0,0 +1,4 @@
+package uz.itschool.telegram_chat.navigation
+
+enum class Screens {
+}
---
+++ app/src/main/java/uz/itschool/telegram_chat/screens/SplashScreen.kt
@@ -0,0 +1,22 @@
+package uz.itschool.telegram_chat.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.R
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+) {}
+
